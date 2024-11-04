c8124d9063fb021ac7871cb7ba17eddddb9667d0
Keval_Pattani <87653668+kevalpattani@users.noreply.github.com>
2024-11-04 12:28:43 +0530

    Update setup.py
    To identify browser by brw values!!

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ def start_setup():
         Looks like you are on Mac OS X
         Are you on an ARM processor? (Apple M Series Chip)
         ''')
+        brw = 1
         if input("[y/n]") == "y":
             d = user_data_read()
             d["driver_name"] = "chromedriver_macos_arm"
@@ -30,15 +31,18 @@ def start_setup():
         d = user_data_read()
         d["driver_name"] = "chromedriver_linux_64"
         user_data_write(d)
+        brw = 2
 
     elif platform == "Windows":
         print("Looks like you are on Windows")
         d = user_data_read()
         d["driver_name"] = "chromedriver_win64.exe"
         user_data_write(d)
+        brw = 3
     else:  
         print("Looks like you are on an unsupported platform")
-
+        brw = 4
+        
     print('\n\n\n')
     username = input("Enter your srn: ")
     password = input("Enter your wifi password: ")
